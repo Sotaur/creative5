0e5cc1fb4e65590b0ccebbe7ed31642997fa8d23
Sotaur <christianmarklambert@gmail.com>
2018-04-14 19:00:21 -0600

    Stubbed

---
+++ src/components/Tickets.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    Tickets
+    <ticket v-for="ticket in this.tickets" > </ticket>
+  </div>
+</template>
+
+<script>
+  import Ticket from './Ticket';
+  export default {
+    components: {
+      Ticket
+    },
+    computed: {
+      tickets: function () {
+        return this.$store.getters.tickets;
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
