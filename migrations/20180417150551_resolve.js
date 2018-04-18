989738fda9e696c48fc9a769747cec4d1ac0ea65
Sotaur <christianmarklambert@gmail.com>
2018-04-17 21:29:12 -0600

    Fixed migrations

--- migrations/20180417150551_resolve.js
+++ migrations/20180417150551_resolve.js
@@ -1,7 +1,7 @@
 exports.up = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('tickets', function(table) {
-      table.varchar('email');
+      table.varchar('resolved');
     }),
   ]);
 };
@@ -9,7 +9,7 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return Promise.all([
     knex.schema.table('tickets', function(table) {
-      table.dropColumn('email');
+      table.dropColumn('resolved');
     }),
   ]);
 };
